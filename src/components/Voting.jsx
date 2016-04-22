fee8d382f89d950cc9ecb8d6ece971116126dafc
spawner999 <lore.ferrario@gmail.com>
2016-04-22 13:40:27 -0700

    add purerenderMixin to all the components

--- src/components/Voting.jsx
+++ src/components/Voting.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import Winner from './Winner.jsx';
 import Vote from './Vote.jsx';
+import PureRenderMixin from 'react-addons-pure-render-mixin';
+
 
 export default React.createClass({
+  mixins: [PureRenderMixin],
   render: function() {
     return <div>
       {this.props.winner ?
