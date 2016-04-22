271a459ddb9adc1da699d35119f128786545904e
spawner999 <lore.ferrario@gmail.com>
2016-04-22 15:50:48 -0700

    add test for reducer and implentation for it too

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Router, Route, hashHistory} from 'react-router';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import reducer from 'reducer';
 import App from './components/App.jsx';
 import Voting from '../src/components/Voting.jsx';
 import Results from './components/Results.jsx'
+
+const store = createStore(reducer);
+store.dispatch({
+  type: 'SET_STATE',
+  state: {
+    vote: {
+      pair: ['Sunshine', '28 Days Later'],
+      tally: {Sunshine: 2}
+    }
+  }
+})
 
 const routes = <Route component={App}>
   <Route path="/results" component={Results} />
