1d4f452355b10d5e2e7045778bda5c3577563150
spawner999 <lore.ferrario@gmail.com>
2016-04-26 10:31:58 -0700

    add socket on client, now able to connect to server

--- src/index.jsx
+++ src/index.jsx
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom';
 import {Router, Route, hashHistory} from 'react-router';
 import {createStore} from 'redux';
 import {Provider} from 'react-redux';
+import io from 'socket.io-client';
 import reducer from './reducer';
 import App from './components/App.jsx';
 import Voting from '../src/components/Voting.jsx';
 import {VotingContainer} from './components/Voting.jsx';
-import Results from './components/Results.jsx'
+import Results from './components/Results.jsx';
+import {ResultsContainer} from './components/results.jsx';
 
 const store = createStore(reducer);
 store.dispatch({
@@ -18,10 +20,12 @@ store.dispatch({
       tally: {Sunshine: 2}
     }
   }
-})
+});
+
+const socket = io(`${location.protocol}//${location.hostname}:8090`);
 
 const routes = <Route component={App}>
-  <Route path="/results" component={Results} />
+  <Route path="/results" component={ResultsContainer} />
   <Route path="/" component={VotingContainer} />
 </Route>;
 
